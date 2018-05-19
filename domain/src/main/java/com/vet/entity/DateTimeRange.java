904122708c7fed29f61786d5f62e0e50700e0263
AlexandraPascu <36105681+AlexandraPascu@users.noreply.github.com>
2018-05-19 19:29:14 +0300

    Added DateTimeRange class

---
+++ domain/src/main/java/com/vet/entity/DateTimeRange.java
@@ -0,0 +1,39 @@
+package com.vet.entity;
+
+import lombok.Getter;
+
+import javax.persistence.Column;
+import javax.persistence.Embeddable;
+import java.time.LocalDateTime;
+
+import static java.time.Duration.between;
+import static java.time.temporal.ChronoUnit.MINUTES;
+
+@Embeddable
+@Getter
+public class DateTimeRange {
+
+    @Column(name = "START")
+    private LocalDateTime start;
+
+    @Column(name = "END")
+    private LocalDateTime end;
+
+    public DateTimeRange(LocalDateTime start, LocalDateTime end) {
+        this.start = start;
+        this.end = end;
+    }
+
+    public DateTimeRange(LocalDateTime start, long durationInMinutes) {
+        this.start = start;
+        this.end = start.plusMinutes(durationInMinutes);
+    }
+
+    public void modifyStart(LocalDateTime newStart) {
+        this.start = newStart;
+    }
+
+    public long getDurationInMinutes() {
+        return between(start, end).get(MINUTES);
+    }
+}
